ec93d05e933f8466af42f13f465e560b8b743fc4
DerekPhil <filip.derewonko@student.uw.edu.pl>
2019-04-02 17:01:11 +0200

    rwd and bugfixes

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import "../src/sass/main.scss";
 import helloWorld from "../src/script.js";
 
 //helloWorld();
+
+const navBtn = document.querySelector(".nav__hamburger");
+const navList = document.getElementById("navList");
+
+navBtn.addEventListener("click", () => {
+    navList.classList.toggle("nav__list--sm-hidden");
+})
